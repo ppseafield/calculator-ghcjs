c09b93eb97d468426bb9248f829d9120a8a89936
Patrick Seafield <pps@ppseafield.info>
2015-08-16 13:48:10 -0700

    Handle operators, equals button

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import Prelude hiding (div, id)
 import Control.Monad (forM_, when)
 import Control.Monad.Trans.Reader 
 import Control.Monad.IO.Class
+import Text.Read (readMaybe)
 
 import GHCJS.DOM
 import GHCJS.DOM.Document
@@ -24,9 +25,12 @@ import qualified Text.Blaze.XHtml5.Attributes as A
 import Text.Blaze.Html.Renderer.String
 
 
+-- | Shorthand for our operator type
+type Operator = Float -> Float -> Float
+
 -- | List of binary operators for our calculator.
 -- Let's stick with floats to keep it simple.
-operators :: [( (Float -> Float -> Float), String )]
+operators :: [( Operator, String )]
 operators = [ ( (+), "plus" )
             , ( (-), "minus" )
             , ( (*), "times" )
@@ -43,7 +47,7 @@ main = runWebGUI $ \webView -> do
   htmlElementSetInnerHTML body $ renderHtml template
 
   Just calc  <- fmap castToHTMLInputElement <$> documentGetElementById doc ("calc-input" :: String)
-  Just total <- fmap castToHTMLElement <$> documentGetElementById doc ("total" :: String)
+  Just divTotal <- fmap castToHTMLElement <$> documentGetElementById doc ("total" :: String)
 
   -- Handle number button clicks
   forM_ [0..9] $ \number -> do 
@@ -53,20 +57,47 @@ main = runWebGUI $ \webView -> do
 
   -- Math Operations
   forM_ operators $ \(operator, name) -> do
-    return ()
+    Just btnOP <- documentGetElementById doc ("btn-" ++ name :: String)
+    elementOnclick btnOP $ clickOperator divTotal calc operator
+
+  Just btnEQ <- documentGetElementById doc ("btn-equals" :: String)
+  elementOnclick btnEQ $ clickEquals divTotal calc
+
 
   -- Backspace and clear all buttons
   Just btnBSP <- documentGetElementById doc ("btn-backspace" :: String)
   elementOnclick btnBSP $ clickBackspace calc
 
   Just btnCC <- documentGetElementById doc ("btn-clear-all" :: String)
-  elementOnclick btnCC $ clickClearAll total calc
+  elementOnclick btnCC $ clickClearAll divTotal calc
 
   return ()
 
 
 
 -- Event handlers
+
+-- | Get the Float value of the calc input and total, then apply the operator to them.
+clickOperator :: HTMLElement -> HTMLInputElement -> Operator -> EventM MouseEvent Element ()
+clickOperator divTotal calc operator = liftIO $ do
+  total   <- htmlElementGetInnerText divTotal :: IO String
+  current <- htmlInputElementGetValue calc :: IO String
+  case (readMaybe total, readMaybe current) :: (Maybe Float, Maybe Float) of
+   ok@(Just t, Just c) -> do
+     htmlElementSetInnerText divTotal $ show (t `operator` c)
+     htmlInputElementSetValue calc  ("" :: String)
+   _ -> putStrLn "Somehow our parse to float failed on calc-input or total. What were you doing?"
+
+-- | Make the current input the total value  
+clickEquals :: HTMLElement -> HTMLInputElement -> EventM MouseEvent Element ()
+clickEquals divTotal calc = liftIO $ do
+  current <- (htmlInputElementGetValue calc :: IO String)
+  case readMaybe current :: Maybe Float of
+   Just c -> do
+     htmlElementSetInnerHTML divTotal $ show c
+     htmlInputElementSetValue calc ("" :: String)
+   _ -> return ()
+
 
 -- | Appends the number to the current calc-input value.
 clickNumber :: HTMLInputElement -> String -> EventM MouseEvent Element ()
@@ -83,10 +114,10 @@ clickBackspace calc = liftIO $ do
 
 
 clickClearAll :: HTMLElement -> HTMLInputElement -> EventM MouseEvent Element ()
-clickClearAll total calc = liftIO $ do
+clickClearAll divTotal calc = liftIO $ do
   current <- (htmlInputElementGetValue calc :: IO String)
   if null current
-    then htmlElementSetInnerHTML total ("0.00" :: String)
+    then htmlElementSetInnerHTML divTotal ("0.00" :: String)
     else htmlInputElementSetValue calc ("" :: String)
 
 
@@ -110,7 +141,9 @@ template = do
     button ! class_ "btn" ! type_ "button" ! id "btn-plus" $ "+"  
     button ! class_ "btn" ! type_ "button" ! id "btn-minus" $ "-"  
     button ! class_ "btn" ! type_ "button" ! id "btn-times" $ "*"  
-    button ! class_ "btn" ! type_ "button" ! id "btn-divide" $ "/"  
+    button ! class_ "btn" ! type_ "button" ! id "btn-divide" $ "/"
+    button ! class_ "btn" ! type_ "button" ! id "btn-equals" $ "="
+
   div $ do
     button ! class_ "btn" ! type_ "button" ! id "btn-7" $ "7"  
     button ! class_ "btn" ! type_ "button" ! id "btn-8" $ "8"  
